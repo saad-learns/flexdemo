be4603a28b7073d5efdfd1378a707e93ac271bf0
Mashrur Mia (Sa'ad) <mashrur.mia@gmail.com>
2022-11-06 23:07:56 +0600

    Remove import

--- src/FlexDemo.cy.jsx
+++ src/FlexDemo.cy.jsx
@@ -1,11 +1,12 @@
 import FlexDemo from './FlexDemo';
-import '@testing-library/cypress/add-commands';
 
 describe('<FlexDemo>', () => {
   it('should switch tab when clicking tab title', () => {
     cy.mount(<FlexDemo />);
+
     cy.findByText('Panel Three').should('not.exist');
     cy.findByText('Three').click();
+
     cy.findByText('Panel Three').should('exist').should('be.visible');
   });
 });
